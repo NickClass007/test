c69abe53598e7396f4b5400be5af24ac8adbb6d5
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-03-24 11:21:07 +0100

    Update common.js

--- common.js
+++ common.js
@@ -13,13 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
   // Event Listener für den Umschalter (Switch)
   toggleSwitch.addEventListener('change', function() {
     if (this.checked) {
-      document.body.classList.add('light-mode');
-      document.body.classList.remove('dark-mode');
-      localStorage.setItem('theme', 'light-mode');
-    } else {
       document.body.classList.add('dark-mode');
       document.body.classList.remove('light-mode');
       localStorage.setItem('theme', 'dark-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light-mode');
     }
   });
 });
