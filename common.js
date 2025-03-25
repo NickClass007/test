dc6440405688cd265dbb1dca6d3289fada0fe9cd
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-03-25 10:31:09 +0100

    Update common.js

--- common.js
+++ common.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const toggleSwitch = document.querySelector('#mode-switch');
-  const currentTheme = localStorage.getItem('theme') || 'light-mode';  // Standardmäßig 'light-mode', wenn nichts gespeichert
+    const toggleSwitch = document.querySelector('#mode-switch');
+    const currentTheme = localStorage.getItem('theme') || 'light-mode'; // Standardmäßig 'light-mode', wenn nichts gespeichert
 
-  // Setze das gespeicherte Thema beim Laden der Seite
-  document.body.classList.add(currentTheme); 
+    // Setze das gespeicherte Thema beim Laden der Seite
+    document.body.classList.add(currentTheme);
 
-  // Setze den Zustand des Schalters (checked oder nicht) basierend auf dem gespeicherten Modus
-  if (currentTheme === 'light-mode') {
-    toggleSwitch.checked = false;  // Light Mode -> Schalter ist nicht aktiviert
-  } else {
-    toggleSwitch.checked = true;   // Dark Mode -> Schalter ist aktiviert
-  }
-
-  // Event Listener für den Umschalter (Switch)
-  toggleSwitch.addEventListener('change', function() {
-    if (this.checked) {
-      document.body.classList.add('dark-mode');
-      document.body.classList.remove('light-mode');
-      localStorage.setItem('theme', 'dark-mode');
+    // Setze den Zustand des Schalters (checked oder nicht) basierend auf dem gespeicherten Modus
+    if (currentTheme === 'light-mode') {
+        toggleSwitch.checked = false;  // Light Mode -> Schalter ist nicht aktiviert
     } else {
-      document.body.classList.add('light-mode');
-      document.body.classList.remove('dark-mode');
-      localStorage.setItem('theme', 'light-mode');
+        toggleSwitch.checked = true;   // Dark Mode -> Schalter ist aktiviert
     }
-  });
+
+    // Event Listener für den Umschalter (Switch)
+    toggleSwitch.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+            document.body.classList.remove('light-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.add('light-mode');
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
 });
+
 
 // Event Listener für den "Dictionary"-Link
 document.getElementById('dictionary-link').addEventListener('click', function() {
